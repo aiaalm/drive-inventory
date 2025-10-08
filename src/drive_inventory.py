64df3a07522dcc038afe8a72bf7980f2a5414080
aiaalm <aiaalm@gmail.com>
2025-10-08 09:03:26 +0200

    Create drive_inventory.py

---
+++ src/drive_inventory.py
@@ -0,0 +1,6 @@
+# Script Python pour inventaire et organisation des fichiers Google Drive
+# TODO: Implémenter la logique
+
+if __name__ == "__main__":
+    print("Script d'inventaire Google Drive")
+    print("Structure à implémenter...")
